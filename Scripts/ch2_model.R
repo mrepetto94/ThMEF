ec5c52bc804b3131e7e7b638b67f5887eb420f84
mrepetto94 <mrepetto94@me.com>
2018-02-14 12:39:59 +0100

    code optimization

--- Scripts/ch2_model.R
+++ Scripts/ch2_model.R
@@ -1,43 +1,63 @@
 
-pricesimulation<-function (pzero, sigma){
+pricesimulation<-function (pzero, sigma, l){
   initialPrice = pzero
   dailyDeviation = sigma
 
   n = 15 #number of days
-
-  prices = vector(mode = "numeric", length = 15)
-  prices = initialPrice + cumsum(rnorm(n = n,mean = 0,sd = dailyDeviation))
+  
+  prices<-vector(mode = "numeric", length = l) 
+  
+  i <- 1
+  for(i in 1:l){  
+    prices[i] = initialPrice + cumsum(rnorm(n = n,mean = 0, sd = dailyDeviation))
+  }
   return(prices)
   }
 
-start<-function(){
-  # Create the bot object
-  bot <- TGBot$new(token = bot_token('RBot'))
-  bot$set_default_chat_id(423034192)
-  # Start routine
-  setwd(here())
-  # Turn off warnings (for XML package)
-  options(warn = -1)
-  # Ping the neos server
-  Nping()
-}
-
 #Main script chapter 2 model
 #Using NEOS server for solving it
-se.seed(5)
 library("rneos")
 library("fPortfolio")
 library("telegram")
 library("here")
 library("ggplot2")
+set.seed(200)
 
-start()
+# Create the bot object
+bot <- TGBot$new(token = bot_token('RBot'))
+bot$set_default_chat_id(423034192)
+# Start routine
+setwd(here())
+# Turn off warnings (for XML package)
+options(warn = -1)
+# Ping the neos server
+Nping()
 
 # Setting the number of simulations
-n <- 5
-# Initialize the matrix with the random prices
+n <- 100
+
+# Commodity data
+Price <- c(15,8,11,6) #price at time zero
+Volatility <-c(0.5,1,2,0.7) #volatility of the commodity on the reference market
+
+# Initialize the matrix with the random prices and the result matrix
+symmat <- matrix(nrow = 4, ncol = n)
+symmat <- t(mapply(pricesimulation, Price, Volatility, ncol(symmat)))
 mainmat <- matrix(nrow = 4, ncol = n)
-i <- 1
+
+# Data File DECLARATION of unchanged variables
+K<-2660
+ProcAct <- c(49, 52, 50, 55) #Variable cost based on the production activities
+demand <- c(1900, 1200, 1600, 600)
+cost = matrix(
+  c(0, 14, 11, 14,
+    27, 0, 12, 22,
+    24, 14, 0, 12,
+    10, 13, 3, 0),
+  nrow=4,
+  ncol=4,
+  byrow = TRUE)
+
 
 # Get the template for the solver
 template<-NgetSolverTemplate(category = "lp", solvername = "MOSEK", inputMethod = "AMPL")
@@ -65,7 +85,7 @@ template<-NgetSolverTemplate(category = "lp", solvername = "MOSEK", inputMethod 
     "s.t. Allocation2{i in ORIG}: sum {j in DEST} Trans[j,i] = supply2[i];"
   )
 
-  # Run File:
+# Run File:
   comf <- c(
     "solve;",
     "display supply;"
@@ -73,45 +93,26 @@ template<-NgetSolverTemplate(category = "lp", solvername = "MOSEK", inputMethod 
     #"display Trans;"
   )
 
-
+i <- 1
 for (i in 1:n){
+  ProcV <- ProcAct + symmat[,i] #add the price forcast to the other costs
+  
   # Data File:
   amplDataOpen("ampl") #clear the dat file
-
-  K<-2660
   amplDataAddValue("K", K, "ampl")
-
-  ProcV <- c(49, 52, 50, 55) #fixed part of variable cost
-  Price <- c(15,8,11,6) #medium price of commodity
-  Volatility <-c(0.5,1,2,0.7) #volatility of the commodity on the reference market
-  #Simulate ProcV as brownian motion
-  Priceforecast <- pricesimulation(10, 0.5)
-  Priceforecast <- mapply(pricesimulation, Price, Volatility)
-  orderlag <- 15 #the time in when we'll do the order
-  ProcV <- ProcV + Priceforecast[orderlag,] #add the price forcast to the other costs
-
-  demand <- c(1900, 1200, 1600, 600)
   amplDataAddVector("ProcV", ProcV, "ampl")
   amplDataAddVector("demand", demand, "ampl")
-
-  cost = matrix(
-    c(0, 14, 11, 14,
-      27, 0, 12, 22,
-      24, 14, 0, 12,
-      10, 13, 3, 0),
-      nrow=4,
-      ncol=4,
-      byrow = TRUE)
   amplDataAddMatrix("cost",cost ,"ampl")
-
+  # Write in the variable 
   datf <- paste(paste(readLines("ampl.dat"), collapse = "\n"), "\n")
 
+  #Pass the AMPL files to the server
   argslist <- list(model = modf, data = datf, commands = comf, comments = "")
   xmls <- CreateXmlString(neosxml = template, cdatalist = argslist)
-
   (test <- NsubmitJob(xmlstring = xmls, user = "mrepetto94", interface = "", id = 0))
 
   #cleaning the result
+  Sys.sleep(2)
   result <- NgetFinalResults(obj = test)
   string <- sub(".*:=\n", "", result@ans)
   string <- sub("\n;\n\n", "", string)
@@ -120,25 +121,16 @@ for (i in 1:n){
   string <- strsplit(string, " ")
   string <- unlist(string)
 
-  mat <- matrix(string, nrow = 4, ncol = 2, byrow = TRUE)
+  mainmat[,i] <- as.numeric(matrix(string, ncol = 2, nrow = 4, byrow = TRUE)[,2])
 
-  mainmat[,i] <- mat[,2]
-
-  if ( (i%%10) == 0){
+  if ( (i%%1) == 0){
   bot$sendMessage(paste("Now processing number ", i))
   }
-  Sys.sleep(5)
-
 
 }
-
 bot$sendMessage(paste("The process is complete, the files are in: ", here()))
-
 write.csv(mainmat, file = "mainmat.csv")
-
-
 storage.mode(mainmat) <- "numeric"
-
 
 png("test.png")
 loc1 <- data.frame(length = mainmat[1,])
@@ -154,6 +146,8 @@ loc4$name <- 'Location4'
 locations <- rbind(loc1, loc2, loc3, loc4)
 
 ggplot(locations, aes(x=length, fill = name)) +
-  geom_histogram( alpha=0.6, position="identity", binwidth = 1)
+  #geom_histogram( alpha=0.6, position="identity")
+  
+
 dev.off()
 bot$sendPhoto("test.png", caption = "Resulting histogram")
